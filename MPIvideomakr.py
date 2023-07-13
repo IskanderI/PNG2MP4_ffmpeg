ce0b5b87745b3e5926cf04683087a8c27f9fcc4a
IskanderI <123633876+IskanderI@users.noreply.github.com>
2023-07-13 14:31:11 +0200

    Update MPIvideomakr.py

--- MPIvideomakr.py
+++ MPIvideomakr.py
@@ -7,16 +7,17 @@ Created on Mon Mar 28 13:54:29 2022
 
 """
 import subprocess,os,time,sys
-# Put the spaces before and after!
-target = " C:/Users/$($Env:UserName)/.julia/conda/3/Scripts/activate.bat "
-cmd    =  " mpiexec -n 6 python3 " + sys.path[0] + "/makevids.py "
 
+def run(cmd): # for windows through powershell
+    result=subprocess.run(["powershell", "-Command Invoke-Expression ", cmd], capture_output=True, text=True)
+    print(result.stdout)
 #======================================
 """
 Import tk for file selection
 to run with dialog windows in windows machines
 # """
 if sys.platform =='win32':
+    ncores = 6
     from tkinter import filedialog as fd
     from tkinter import Tk
     win        = Tk()
@@ -29,30 +30,22 @@ if sys.platform =='win32':
 else:
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("Folder", help="Folder of the Tests",type=str)
-    parser.add_argument("ptsave",help="Folder where to create the output",type=str)
-    args = parser.parse_args()
-    dirselect = args.Folder
+    parser.add_argument("ncores", help="Amount of cores",type=int)
+    parser.add_argument("Folder", help="Folder of the images",type=str)
+    parser.add_argument("ptsave",help="Folder where to put the output",type=str)
+    args       = parser.parse_args()
+    dirselect  = args.Folder
     saveselect = args.ptsave
+    ncores     = args.ncores
 
 # ===================================
 
-# start_time = time.time()
-
-#dirselect  = 'F:/Iskander/Lamem_models/2023/pics/tesmakevids/pics/'
-#saveselect = "F:/Iskander/Lamem_models/2023/pics/tesmakevids/vids/"
-
-def run( cmd):
-    if sys.platform =='win32':
-        result=subprocess.run(["powershell", "-Command Invoke-Expression ", cmd], capture_output=True, text=True)
-        print(result.stdout)
+# Put the spaces before and after!
+target = " C:/Users/$($Env:UserName)/.julia/conda/3/Scripts/activate.bat "
+cmd    =  " mpiexec -n " + ncores +" python3 " + sys.path[0] + "/makevids.py "
 
 if sys.platform =='win32':
     run(target)
     run(cmd+dirselect+' '+saveselect)
 else:
-    os.system(cmd  + dirselect + ' ' + saveselect )
-
-# time.sleep(2)
-# print("\n \n Time spent for calculation : --- %s seconds ---"  %(time.time() - start_time))
-  
+    os.system(cmd  + dirselect + ' ' + saveselect ) 
